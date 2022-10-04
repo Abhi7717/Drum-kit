65026552c8582c8bb4afcb19dfd2127f97aad76a
Abhi7717 <95903021+Abhi7717@users.noreply.github.com>
2022-10-04 10:52:59 +0530

    Add files via upload

---
+++ drumindex.js
@@ -0,0 +1,85 @@
+// document.querySelectorAll("button")[0].addEventListener("click",function(){
+
+//     var buttonInnerHTML=this.innerHTML;
+//     switch (buttonInnerHTML) {
+//         case "w":
+//             let tom1=new Audio("sounds/tom-1.mp3");
+//             tom1.play();
+//             break;
+//             case "a":
+//              let tom2=new Audio("sounds/tom-2.mp3");
+//                 tom2.play();
+//              break;
+//              case "s":
+//             let tom3=new Audio("sounds/tom-3.mp3");
+//               tom3.play();
+//             break;
+//             case "d":
+//             let tom4=new Audio("sounds/tom-4.mp3");
+//               tom4.play();
+//             break;
+//             case "j":
+//             let snare=new Audio("sounds/snare.mp3");
+//               snare.play();
+//             break;
+//             case "k":
+//             let crash=new Audio("sounds/crash.mp3");
+//               crash.play();
+//             break;
+//             case "l":
+//             let kick=new Audio("sounds/kick-bass.mp3");
+//               kick.play();
+//             break;
+//         default:
+//             break;
+//     }
+// });
+
+
+function tom1()
+{
+    let tom1=new Audio("sounds/tom-1.mp3");
+                tom1.play();
+            
+}
+
+function tom2()
+{
+    let tom2=new Audio("sounds/tom-2.mp3");
+                tom2.play();
+}
+
+function tom3()
+{
+    let tom3=new Audio("sounds/tom-3.mp3");
+                tom3.play();
+    // prompt("enter the numebr a");
+    // let a=1,b=2,s=0,d;
+    // a=parseInt(prompt("enter the 1st number"));
+    // b=parseInt(prompt("enter the 2nd number"));
+    // // <input type="number"></input>
+    // s=a+b;
+    // // d=a-b;
+    //  console.log(s);
+    //  alert(s);
+}
+function tom4()
+{
+    let tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+}
+function crash()
+{
+    let crash=new Audio("sounds/crash.mp3");
+            crash.play();
+}
+function snare()
+{
+    let snare=new Audio("sounds/snare.mp3");
+                snare.play();
+}
+function kick()
+{
+    let kick_bass = new Audio("sounds/kick-bass.mp3");
+                kick_bass.play();
+}
